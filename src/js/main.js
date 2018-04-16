6c04f66efaae46b99433806bdb917639069a715e
Vlad Tepes <vg.email.2@gmail.com>
2018-04-16 23:51:58 +0300

    slideshow added

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
+$(document).ready(function () {
+
     //----------------------<<svg-for-ie>>----------------------\\
     (function () {
         svg4everybody();
     }());
+
+    //----------------------<<slideshow>>----------------------\\
+    (function () {
+        $('.block__link').on('click', function (e) {
+            e.preventDefault();
+
+            var
+                $this = $(this),
+                container = $this.closest('.career'),
+                display = container.find('.career__pic-img'),
+                item = $this.closest('.career__item'),
+                path = $this.attr('href'),
+                duration = 500,
+                preloader = $('.preloader');
+
+            if (!item.hasClass('active')) {
+                preloader.show();
+
+                display.fadeOut(duration, function () {
+                    $(this).attr('src', path).on('load', function () {
+                        preloader.hide();
+                        $(this).fadeIn(duration);
+                    });
+                });
+
+                item.addClass('active')
+                    .siblings()
+                    .removeClass('active');
+            }
+        });
+    }());
+});
